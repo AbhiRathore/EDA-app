d5ae6f36f29f3d430392968a26dbf1b8c937ed0c
AbhiRathore <abhishekvishnu1988@gmail.com>
2020-08-23 18:30:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -138,4 +138,4 @@ def update_data(chosen_rows,piedropval,linedropval):
 #------------------------------------------------------------------
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run_server(debug=True)
